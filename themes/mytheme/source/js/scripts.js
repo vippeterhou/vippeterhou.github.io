a0b9a13ee8de2b29e47790915848eb459340cde0
vippeterhou <vippeterhou@gmail.com>
2022-04-11 01:52:12 +0800

    loading gif

--- themes/mytheme/source/js/scripts.js
+++ themes/mytheme/source/js/scripts.js
@@ -1,6 +1,7 @@
 // global variables
 let body;
 let moonsun;
+let mainimg;
 
 // do it when DOM is ready. It doesn't mean images are ready.
 document.addEventListener("DOMContentLoaded", this.readDarkLightModeCookie)
@@ -9,7 +10,6 @@ function prepareElements () {
     body = document.getElementsByTagName("body")[0];
     moonsun = document.getElementById("moon-sun");
 }
-
 function toggleDarkLightMode () {
     // toggle class "dark" for body
     body.classList.toggle("dark")
@@ -18,7 +18,6 @@ function toggleDarkLightMode () {
     moonsun.children[0].classList.toggle("fa-moon");
     moonsun.children[0].classList.toggle("fa-sun");
 }
-
 function readDarkLightModeCookie() {
     prepareElements();
     const isDarkMode = window.localStorage.getItem("phhp-theme") == "dark";
@@ -37,11 +36,42 @@ function switchDarkLightMode() {
     }
 }
 
-(function fetchMotto() {
+// window.addEventListener("load", event => {
+//     const mainimg = document.getElementById('mainimg');
+//     if (mainimg) {
+//         load = mainimg.complete;
+//         if (load) {
+//             console.log("main img is ready");
+//         }
+//     }
+// });
+
+// function isHomePage() {
+//     mainimg = document.getElementById('mainimg');
+//     if (mainimg) {
+//         return true;
+//     } else {
+//         return false;
+//     }
+// }
+
+async function loading(){
+    return new Promise(function(resolve, reject) {
+        setTimeout(function(){
+            const loader = document.getElementById("loading-container");
+            loader.className = "fadeout"; // fade out the loading page
+            resolve("loaded.");
+        },1000); // show loading page 1s at least
+    })
+};
+
+function fetchMotto() {
     jinrishici.load(function(result) {
     const mottoElement = document.querySelector("#motto")
+
+    if (!mottoElement) return
+
     let text = result.data.content
-    // mottoElement.innerHTML = text;
 
     // print effect
     if (text.charAt(text.length - 1) === '。') {
@@ -55,4 +85,10 @@ function switchDarkLightMode() {
         mottoElement.innerText = (text.slice(0, i++));
     }, 100);
   });
+};
+
+(function () {
+    loading().then(
+        () => { fetchMotto(); }
+    );
 })();
